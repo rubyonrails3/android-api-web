8e4ee46820ebc3e03ee300875f577e96958d829e
Ismail Akram <rubyonrails3@gmail.com>
2015-08-20 01:44:41 +0500

    Migrations will work now my cap migrate command

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :stage, :production
 
 
-server '128.199.125.71', user: 'deploy', port: 22, roles: %w{web app}
+server '128.199.125.71', user: 'deploy', port: 22, roles: %w{web app db}
 
 set :ssh_options, {
   forward_agent: true,
