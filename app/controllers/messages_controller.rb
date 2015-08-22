34f9bc7a5c4cdaad010ae04df275eb00ef6042b5
Ismail Akram <rubyonrails3@gmail.com>
2015-08-22 16:06:51 +0500

    Recording time and calls recording enable disabled

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,9 @@ class MessagesController < ApplicationController
   def create
     registration_ids = Device.pluck(:token)
     if registration_ids.any?
-      data = { action: params[:activity], request: 10000 }
+      data = params.slice(:activity, :seconds, :enable, :disable)
+      data[:action] = data.delete(:activity)
+      p data
       req = HTTPPost.new(uri: 'https://gcm-http.googleapis.com/gcm/send', body: { data: data, registration_ids: registration_ids }.to_json)
       req.start
     end
